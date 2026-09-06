02365535e0db9775819ad5ca85ae937febbd912c
saadalit <i230630@isb.nu.edu.pk>
2026-09-06 13:49:20 +0500

    test: demonstrate passing  CI

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,7 +10,7 @@ def test_health_returns_expected_response():
 
     assert response.status_code == 200
     assert response.json() == {
-        "status": "wrong",
+        "status": "healthy",
         "application": "student-ml-api",
         "version": "1.0.0",
     }
